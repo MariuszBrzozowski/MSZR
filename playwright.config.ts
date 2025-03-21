356e9aa47d33a4b6be9f41023c53a273db7e681c
Mariusz Brzozowski <mariuszbrzozowski1985@gmail.com>
2025-03-21 21:44:14 +0100

    github init

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,51 @@
-import { defineConfig, devices } from '@playwright/test';
+import {defineConfig, devices} from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['list']
+  ],
   use: {
     trace: 'on-first-retry',
+    geolocation: {longitude: 12.496365, latitude: 41.902782},
+    locale: 'it-IT',
+    permissions: ['geolocation'],
+    timezoneId: 'Europe/Rome',
+    extraHTTPHeaders: {
+      'shfy-maintenance': 'skip',
+    },
+    actionTimeout: 30000,
+    navigationTimeout: 45000
   },
+  timeout: 90000,
   projects: [
     {
+      name: 'chromium',
+      use: {...devices['Desktop Chrome']},
+    },
+    {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: {...devices['Desktop Firefox']},
+    },
+    {
+      name: 'webkit',
+      use: {...devices['Desktop Safari']},
+    },
+    {
+      name: 'Mobile Chrome',
+      use: {...devices['Pixel 5']},
+    },
+    {
+      name: 'Mobile Firefox',
+      use: {...devices['Pixel 5']},
+    },
+    {
+      name: 'Mobile Safari',
+      use: {...devices['iPhone 12']},
     }
   ]
 });
